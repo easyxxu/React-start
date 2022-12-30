927621356c6f81f6b676d44c32c6cf523949c283
easyxxu <107910342+easyxxu@users.noreply.github.com>
2022-12-30 13:12:49 +0900

    초기세팅

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [money, setMoney] = useState();
+  const [coinprice, setCoinprice] = useState();
+  const [symbol, setSymbol] = useState();
+  const onChange = (event) => {
+    setMoney(event.target.value);
+  };
+  const handleCoin = (event) => {
+    setCoinprice(event.target.value);
+    setSymbol(event.target.title);
+    console.log(event.target.title);
+  };
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json); //api를 coins[]에 저장
+        setLoading(false);
+      });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>The Coins! ({loading ? "" : coins.length})</h1>
+      {loading ? (
+        "Loading..."
+      ) : (
+        <select onChange={handleCoin}>
+          <option>Select Coin</option>
+          {coins.map((coin) => (
+            <option
+              key={coin.id}
+              value={coin.quotes.USD.price}
+              title={coin.symbol}
+            >
+              {coin.name} ({coin.symbol}) : $ {coin.quotes.USD.price}
+            </option>
+          ))}
+        </select>
+      )}
+      <br />
+      <br />
+      <label htmlFor="money">your money </label>
+      <input
+        id="money"
+        onChange={onChange}
+        type="number"
+        placeholder="Write money you have"
+        value={money}
+      />
+      <h2>
+        You can get {money / coinprice} {symbol}
+      </h2>
+      <p>{symbol}</p>
     </div>
   );
 }
