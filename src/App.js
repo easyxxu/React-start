80b0e9990d4ccf9be19e30da8c6ff9f59bb514f2
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-01-02 18:32:27 +0900

    #7.6 Parameters

--- src/App.js
+++ src/App.js
@@ -1,4 +1,18 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 function App() {
-  return null;
-} // router를 render한다. router는 URL을 보고있는 component
+  return (
+    <Router>
+      <Switch>
+        <Route path="/movie/:id">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
 export default App;
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,34 @@
+import { useCallback, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 function Detail() {
-  return <h1>Detail</h1>;
+  const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState([]);
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setMovie(json.data.movie);
+    setLoading(false);
+    console.log(json);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+  console.log(movie);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div key={movie.id}>
+          <img src={movie.medium_cover_image} alt="movie_cover" />
+          <h2>"{movie.title}"</h2>
+          <p>{movie.description_full}</p>
+        </div>
+      )}
+    </div>
+  );
 }
 export default Detail;
